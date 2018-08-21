9087fa375a63d1f626c246f35fc29ee7ffafd9da
miniBear <ax_zaa@windowslive.com>
2018-08-21 16:14:11 +0700

    test

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World! osadasioiohuguodased'
+  return 'Hello, World! allNEW'
 
 if __name__ == '__main__':
   app.run()
